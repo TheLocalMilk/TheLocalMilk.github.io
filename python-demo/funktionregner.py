b6f0899e99ebf1a1f261295d82af981ade57abf3
Noah <noah.linde@outlook.com>
2022-09-20 08:30:24 +0200

    Upload Opgave

---
+++ python-demo/funktionregner.py
@@ -0,0 +1,8 @@
+#for i in range(0,11,1):
+#    x = i
+#    y =(3*(x*x)) + (6*x) + 9
+#    print(y)
+for i in range(0,110,10):
+    x = i
+    y =(3*(x*x)) + (6*x) + 9
+    print(y)
---
+++ python-demo/tabeler.py
@@ -0,0 +1,20 @@
+for i in range(2, 22, 2):
+    print(i)
+for i in range(3, 33, 3):
+    print(i)
+for i in range(4, 44, 4):
+    print(i)
+for i in range(5, 55, 5):
+    print(i)
+for i in range(5, 55, 5):
+    print(i)
+for i in range(6, 66, 6):
+    print(i)
+for i in range(7, 77, 7):
+    print(i)
+for i in range(8, 88, 8):
+    print(i)
+for i in range(9, 99, 9):
+    print(i)
+for i in range(10, 110, 10):
+    print(i)
